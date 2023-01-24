a72134b7af9d810cd5b2033f9800a4269cd126ca
Chris <37954566+codingnuclei@users.noreply.github.com>
2023-01-24 14:48:33 +0000

    fix: return void to onclick function

--- src/components/auth-showcase.tsx
+++ src/components/auth-showcase.tsx
@@ -8,13 +8,12 @@ export const AuthShowcase: React.FC = () => {
   //   { enabled: session?.user !== undefined }
   // );
   if (session) {
- 
     return (
       <>
         Signed in as {session?.user?.name} <br />
         <button
           className="mr-4 rounded-md bg-white px-4 py-2 duration-200 ease-in-out hover:bg-light-blue hover:text-white"
-          onClick={() => signOut({ callbackUrl: "signOut" })}
+          onClick={() => void signOut({ callbackUrl: "signOut" })}
         >
           Sign out
         </button>
@@ -26,7 +25,7 @@ export const AuthShowcase: React.FC = () => {
       Not signed in <br />
       <button
         className="mr-4 rounded-md bg-white px-4 py-2 duration-200 ease-in-out hover:bg-light-blue hover:text-white"
-        onClick={() => signIn("cognito")}
+        onClick={() => void signIn("cognito")}
       >
         Sign in
       </button>
